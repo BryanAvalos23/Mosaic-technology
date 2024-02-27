448db6495f5fcb90b1981265f648e9bc2d05c59d
Bryan Avalos <trollerepik@gmail.com>
2024-02-27 04:22:11 -0600

    feat: add new dependencie and more content
    - Se agrego el archivo team.tx con los integrantes.
    - Se agrego el archivo Team.jsx
    - Se agrego la dependencia swiper
    - Se agrego archivo css custom para el slider

---
+++ public/js/team.ts
@@ -0,0 +1,39 @@
+const TEAM = [
+  {
+    name: 'David Eduardo Henriquez Garay',
+    image: '',
+    rol: 'CEO',
+    linkedin: '',
+    website: ''
+  },
+  {
+    name: 'Bryan Edenilson Avalos Sanchez',
+    image: '',
+    rol: 'CTO',
+    linkedin: 'https://www.linkedin.com/in/bryan-avalos-228286273/',
+    website: ''
+  },
+  {
+    name: 'Dennis Ernesto Saravia Cruz',
+    image: '',
+    rol: 'CCO',
+    linkedin: 'https://www.linkedin.com/in/dennis-saravia/',
+    website: ''
+  },
+  {
+    name: 'Christian Adonay Macias Nuñez',
+    image: '',
+    rol: 'CMO',
+    linkedin: '',
+    website: ''
+  },
+  {
+    name: 'Osmar Xavier Cortez Chicas',
+    image: '',
+    rol: 'CFO',
+    linkedin: 'https://www.linkedin.com/in/xavier-cortez-338781268/',
+    website: ''
+  }
+];
+
+export default TEAM;
