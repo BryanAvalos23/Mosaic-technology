9fac96cc8c895c3c84e264ac85cb59d7c59994e7
Bryan Avalos <trollerepik@gmail.com>
2024-02-26 19:38:15 -0600

    feat: refactor in Services.astro
    Se realizo una refactorizacion de codigo en el archivo Services.astro,
    se movio todo el arreglo de objetos a un archivo llamado services en la
    carpeta public/js

---
+++ public/js/services.ts
@@ -0,0 +1,22 @@
+const SERVICES = [
+  {
+    title: 'Desarrollo de software',
+    description:
+      'Desarrollo de software a medida para las diferentes plataformas: web, móvil y desktop.',
+    img: '/images/software_services.jpg',
+  },
+  {
+    title: 'Infraestructura TI',
+    description:
+      'Diseño e implementación de infraestructura tecnológica: redes, servidores y cloud computing.',
+    img: '/images/infraestructure_services.jpg',
+  },
+  {
+    title: 'Asesoria TI',
+    description:
+      'Servicios de asesoría donde buscamos guiarte en la búsqueda e implementación de lo que realmente necesitas tú y tu negocio.',
+    img: '/images/Asesoria_services.jpg',
+  },
+];
+
+export default SERVICES;
