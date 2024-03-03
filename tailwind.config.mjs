95f431d3d629dbdd9b29af6e5d096b18bc261bf4
Bryan Avalos <trollerepik@gmail.com>
2024-03-02 18:47:20 -0600

    change: screen change size

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,12 +6,7 @@ export default withMT({
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
-      screens: {
-        'sm': '320px',
-        'md': '640px',
-        'lg': '768px',
-        'xl': '1024px'
-      }
+
     },
   },
   plugins: [],
