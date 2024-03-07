e6e499c7a51296f9f219bd812f60a769bd3d1427
Bryan Avalos <trollerepik@gmail.com>
2024-03-07 16:00:24 -0600

    feat: add filter services limited
    - Se agrego el codigo de js para poder filtrar los servicios y
      mostrarlos en el dom

--- public/js/services.ts
+++ public/js/services.ts
@@ -1,22 +1,64 @@
 const SERVICES = [
   {
-    title: 'Desarrollo de software',
+    title: 'Consultoria y Asesoria tecnologica',
     description:
-      'Desarrollo de software a medida para las diferentes plataformas: web, móvil y desktop.',
+      'Descubre las diferentes opciones de asesoria que tenemos, buscando la manera de guiarte a tomar una mejor decision en tu adquisicion tecnologica',
     img: '/images/software_services.jpg',
   },
   {
-    title: 'Infraestructura TI',
+    title: 'Soluciones Cloud y Migracion',
     description:
-      'Diseño e implementación de infraestructura tecnológica: redes, servidores y cloud computing.',
+      'Todo lo necesario para crear tu nueva infraestructura en la nube o migrar tu infraestructura fisica actual a la nube',
     img: '/images/infraestructure_services.jpg',
   },
   {
-    title: 'Asesoria TI',
+    title: 'Desarrollo de soluciones digitales',
     description:
-      'Servicios de asesoría donde buscamos guiarte en la búsqueda e implementación de lo que realmente necesitas tú y tu negocio.',
+      'Llevamos tus ideas a cabo, creando software a medida para ti, diversidad de apliaciones para diferentes entornos',
     img: '/images/Asesoria_services.jpg',
   },
+  {
+    title: 'Comercio electronico y Sistemas POS',
+    description:
+      'Integra soluciones de comercio electronico y sistemas de punto de venta para que tu negocio siga creciendo',
+    img: '/images/Asesoria_services.jpg',
+  },
+  {
+    title: 'Infraestructura y soporte TI',
+    description:
+      'Diversidad de servicios de infraestructura fisica, virtualizacion, monitoreo de rede y mas. Para que operes con optimas condiciones',
+    img: '/images/Asesoria_services.jpg',
+  },
+  {
+    title: 'Capacitaciones y concientizacion tecnologica',
+    description:
+      'Te brindamos capacitaciones tecnologicas y talleres de concientizacion de seguridad informatica',
+    img: '/images/Asesoria_services.jpg',
+  },
+  {
+    title: 'Alisis de datos y Bussines Inteligence',
+    description:
+      'Implementacion de herramientas de BI para realizar analisis de datos teniendo asi mejor toma de decisiones',
+    img: '/images/Asesoria_services.jpg',
+  },
+  {
+    title: 'Colaboracion y productividad offline y online',
+    description:
+      'Diversidad de implementaciones para automatizar los procesos de tu negocio y una mejor productividad de tu equipo de trabajo',
+    img: '/images/Asesoria_services.jpg',
+  },
+  {
+    title: 'Diseño de experiencia de usuario(UI/UX)',
+    description:
+      'Servicios de diseño de interfaces intuitivas y atractivas para diferentes tipo de aplicaciones',
+    img: '/images/Asesoria_services.jpg',
+  },
+  {
+    title: 'Solucion modular personalizada',
+    description:
+      'Una plataforma que se adapta y crece junto a ti!. Elije lo qeu necesitas y listo',
+    img: '/images/Asesoria_services.jpg',
+  }
 ];
 
 export default SERVICES;
